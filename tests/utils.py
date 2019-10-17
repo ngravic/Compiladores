0c3093e544800756a3f3bf34dd8b7d3e5c0b479f
nicolas <nicolas@nicolas-thinkpad.thinkpad>
2019-10-17 12:50:17 -0300

    fix path tiger

--- tests/utils.py
+++ tests/utils.py
@@ -6,12 +6,16 @@ def getAllTigerFiles(folder):
         for r, _, f in os.walk(folder):
             for file in f:
                 if '.tig' in file:
-                    files.append(os.path.join(r, file))        
+                    files.append(os.path.abspath(os.path.join(r, file)))        
         return files
 
 def runTest(file):
     TIGER_PATH = os.path.abspath(os.path.join(os.path.abspath('.'), '..', 'tiger'))
-    p = subprocess.Popen([TIGER_PATH, file], stdout=subprocess.PIPE, shell=True)
+    p = subprocess.Popen([TIGER_PATH, file], stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE)
     (out, err) = p.communicate()
-    p.wait()
-    return out
+    status = p.wait()
+    if status == 0:
+        return out
+    else:
+        return err    
